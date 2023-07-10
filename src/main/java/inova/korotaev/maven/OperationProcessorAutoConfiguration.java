ec2d0e0540078ea73823556183a7d8948985dc83
sykorotaye <sykorotaye@mts.ru>
2023-07-10 10:59:36 +0300

    some refactoring, added shell for SearchParam

--- src/main/java/inova/korotaev/maven/OperationProcessorAutoConfiguration.java
+++ src/main/java/inova/korotaev/maven/OperationProcessorAutoConfiguration.java
@@ -1,11 +1,7 @@
 package inova.korotaev.maven;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-@EnableAutoConfiguration
 public class OperationProcessorAutoConfiguration {
-    public static void main(String[] args) {
-    }
 }
